95916147759dc2ed73df4a94e287a88becc4498f
coligado <coligado@cs.stanford.edu>
2016-03-05 20:17:53 -0800

    refactoredr

--- CLCSFast.py
+++ CLCSFast.py
@@ -10,37 +10,16 @@ def main ():
     for line in f: 
       print line
       A,B = line.split()
-      CLCS_fast(A,B)
+      single_shortest_path(A,B)
       return
 
 
-def CLCS_fast(A,B):
-  A = 'ABCBDAB' # test string
-  B = 'BDCABA' # test string
-
+def set_up(A,B):
   double_A = A + A
   m = len(double_A)
   n = len(B)
+  p = []
 
-  length_table = numpy.zeros(shape=(m,n)) #c
-  direction_table = numpy.chararray((m, n))
-  direction_table[:] = '0'
-
-  for (x,y), value in numpy.ndenumerate(direction_table):
-    if x >= 1 and y >= 1:
-      if double_A[x] == B[y]:
-        length_table[x,y] = length_table[x-1,y-1] + 1
-        direction_table[x,y] = '\\'
-      elif length_table[x-1,y] >= length_table[x,y-1]:
-        length_table[x,y] = length_table[x-1,y]
-        direction_table[x,y] = '^'
-      else:
-        direction_table[x,y] = '<'
-
-  # create something that represents the path?
-  # allocate storage for the paths p_i
-  # compute p_0 using the whole table
-  return
 
 def find_shortest_paths(A,B,p,l,u):
   if u - l <= 1:
@@ -51,9 +30,49 @@ def find_shortest_paths(A,B,p,l,u):
   find_shortest_paths(A,B,p,mid,u)
 
 
-def cut (shorter, i):
-  new_str = shorter[i:] + shorter[:i]
-  return new_str
+def single_shortest_path(A,B):
+  A = 'ABCBDAB' # test string, later change the string to be that bounded by the table
+  B = 'BDCABA' # test string
+
+  m = len(A) + 1
+  n = len(B) + 1
+
+  length_table = numpy.zeros(shape=(m,n)) #c
+  direction_table = numpy.chararray((m, n))
+  direction_table[:] = '0'
+
+  for (x,y), value in numpy.ndenumerate(direction_table):
+    if x >= 1 and y >= 1:
+      if A[x-1] == B[y-1]:
+        length_table[x,y] = length_table[x-1,y-1] + 1
+        direction_table[x,y] = '\\'
+      elif length_table[x-1,y] >= length_table[x,y-1]:
+        length_table[x,y] = length_table[x-1,y]
+        direction_table[x,y] = '^'
+      else:
+        length_table[x,y] = length_table[x,y-1]
+        direction_table[x,y] = '<'
+
+  print direction_table
+  print length_table
+  print_LCS(direction_table,A,len(A),len(B))
+
+  # create something that represents the path?
+  # allocate storage for the paths p_i
+  # compute p_0 using the whole table
+  return
+
+
+def print_LCS(direction_table,A,x,y):
+  if x == 0 or y == 0:
+    return
+  if direction_table[x,y] == '\\':
+    print_LCS(direction_table,A,x-1,y-1)
+    print A[x-1]
+  elif direction_table[x,y] == '^':
+    print_LCS(direction_table,A,x-1,y)
+  else:
+    print_LCS(direction_table,A,x,y-1)
 
 
 if __name__ == '__main__':
