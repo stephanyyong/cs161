de42aa97b677b95e3b3d60e86f3a8ffe69533f5f
coligado <coligado@cs.stanford.edu>
2016-03-06 23:45:27 -0800

    fixed some bags

--- CLCSFast.py
+++ CLCSFast.py
@@ -9,29 +9,38 @@ def main ():
   with open(sys.argv[1]) as f: 
     for line in f: 
       A,B = line.split()
-      print "Paths for " + line
-      set_up(A,B)
+      print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
+      print '\nPaths for ' + A + ' x ' + B
+      preallocate_table(A,B)
+      return
 
 
-def set_up(A,B):
-  double_A = A + A
-  double_m = len(double_A)
+def preallocate_table(A,B):
+  global preallocated_table
   m = len(A)
   n = len(B)
-  p = ['' for x in range(m)]
-  # p[0] = single_shortest_path(double_A,B,0,m)
-  # p[m-1] = single_shortest_path(double_A,B,m-1,double_m)
-  for r in range(m):
-    p[r] = single_shortest_path(double_A,B,r,r+m)
-  # find_shortest_paths(double_A,B,p,0,m)
-  print p
-  max_subsequence = max(p, key=len)
-  print "The largest subsequence is " + max_subsequence
-  print len(max_subsequence)
-  print '\n'
+  double_A = A + A
+  double_m = len(double_A)
+  
+  # initialize the table of paths
+  preallocated_table = numpy.chararray((double_m+1, n+1))
+  preallocated_table[:] = '0'
+  for x in range(n):
+    preallocated_table[0,x+1] = B[x]
+  for y in range(double_m):
+    preallocated_table[y+1,0] = A[y%m]
+
+  # initialzie the list of paths p_i
+  p = [[] for i in range(m)]
+  p[0] = single_shortest_path(p,0,double_A,B,m,-1,-1)
+  p[m-1] = single_shortest_path(p,m-1,double_A,B,m,-1,-1)
+  find_shortest_paths
+  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  for i in p:
+    print i
 
 
-def find_shortest_paths(A,B,p,l,u):
+def find_shortest_paths(A,B,p,u,l):
   if u - l <= 1:
     return
   mid = (u+l)/2
@@ -40,12 +49,13 @@ def find_shortest_paths(A,B,p,l,u):
   find_shortest_paths(A,B,p,mid,u)
 
 
-def single_shortest_path(A,B,lower_bound,upper_bound):
-  A = A[lower_bound:upper_bound] # should have the same length as original A
+def single_shortest_path(p,mid,double_A,B,original_height,upper_bound,lower_bound):
+  global preallocated_table
+  A = double_A[mid:mid+original_height]
   m = len(A) + 1
   n = len(B) + 1
 
-  length_table = numpy.zeros(shape=(m,n)) #c
+  length_table = numpy.zeros(shape=(m,n))
   direction_table = numpy.chararray((m, n))
   direction_table[:] = '0'
 
@@ -60,17 +70,31 @@ def single_shortest_path(A,B,lower_bound,upper_bound):
       else:
         length_table[x,y] = length_table[x,y-1]
         direction_table[x,y] = '<'
-  # LCS = []
-  # print_LCS(direction_table,A,len(A),len(B),LCS)
-  LCS = print_LCS_nonrecursively(direction_table,A,x,y)
-  print "LCS for " + A + " and " + B
-  print LCS
-  return LCS
+
+  p_mid = print_LCS_nonrecursively(direction_table,A,m-1,n-1,mid)
+  for value in p_mid:
+    preallocated_table[value[0],value[1]] = str(mid+1)
+  for x in range(n-1):
+    direction_table[0,x+1] = B[x]
+  for y in range(m-1):
+    direction_table[y+1,0] = A[y%m]
+  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  print 'p_mid:'
+  print p_mid
+  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  print 'direction_table:'
+  print direction_table
+  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  print 'preallocated_table:'
+  print preallocated_table
+  return p_mid
 
 
-def print_LCS_nonrecursively(direction_table,A,x,y):
+def print_LCS_nonrecursively(direction_table,A,x,y,mid):
+  path_i = []
   subsequence = ''
   while x != 0 and y != 0:
+    path_i.append((x+mid,y))
     if direction_table[x,y] == '\\':
       subsequence += A[x-1]
       x = x - 1
@@ -79,7 +103,7 @@ def print_LCS_nonrecursively(direction_table,A,x,y):
       x = x - 1
     else:
       y = y - 1
-  return subsequence
+  return path_i
 
 
 def print_LCS(direction_table,A,x,y,LCS):
