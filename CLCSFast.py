95c0282bde9157a6da22f243dfa7254f8c917ffc
coligado <coligado@cs.stanford.edu>
2016-03-07 11:28:09 -0800

    Recursion seems to be working

--- CLCSFast.py
+++ CLCSFast.py
@@ -9,18 +9,23 @@ def main ():
   with open(sys.argv[1]) as f: 
     for line in f: 
       A,B = line.split()
-      print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
       print '\nPaths for ' + A + ' x ' + B
+      print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
       preallocate_table(A,B)
       return
 
 
 def preallocate_table(A,B):
   global preallocated_table
+  global double_A
+  global original_B
+  global m
+  global n
   m = len(A)
   n = len(B)
   double_A = A + A
   double_m = len(double_A)
+  original_B = B
   
   # initialize the table of paths
   preallocated_table = numpy.chararray((double_m+1, n+1))
@@ -32,36 +37,47 @@ def preallocate_table(A,B):
 
   # initialzie the list of paths p_i
   p = [[] for i in range(m)]
-  p[0] = single_shortest_path(p,0,double_A,B,m,-1,-1)
-  p[m-1] = single_shortest_path(p,m-1,double_A,B,m,-1,-1)
-  find_shortest_paths
-  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  p[0] = single_shortest_path(p,0,-1,-1)
+  p[m-1] = single_shortest_path(p,m-1,-1,-1)
+  find_shortest_paths(p,0,m-1)
+  print '\nHere are all paths:'
+  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
   for i in p:
     print i
 
 
-def find_shortest_paths(A,B,p,u,l):
-  if u - l <= 1:
+def find_shortest_paths(p,upper_bound,lower_bound):
+  print 'find_shortest_paths with u: ' + str(upper_bound) + ' and l: ' + str(lower_bound)
+  if lower_bound - upper_bound <= 1:
     return
-  mid = (u+l)/2
-  p[mid] = single_shortest_path(A,B,l,u)
-  find_shortest_paths(A,B,p,l,mid)
-  find_shortest_paths(A,B,p,mid,u)
+  mid = (upper_bound+lower_bound)/2
+  p[mid] = single_shortest_path(p,mid,upper_bound,lower_bound)
+  find_shortest_paths(p,mid,lower_bound)
+  find_shortest_paths(p,upper_bound,mid)
 
 
-def single_shortest_path(p,mid,double_A,B,original_height,upper_bound,lower_bound):
+def single_shortest_path(p,mid,upper_bound,lower_bound):
   global preallocated_table
-  A = double_A[mid:mid+original_height]
-  m = len(A) + 1
-  n = len(B) + 1
+  global double_A
+  global original_B
+  global m
+  global n
+  A = double_A[mid:mid+m]
 
-  length_table = numpy.zeros(shape=(m,n))
-  direction_table = numpy.chararray((m, n))
+  if (upper_bound >= 0):
+    print 'This is the upper_bound:'
+    print p[upper_bound]
+  if (lower_bound >= 0):
+    print 'This is the lower_bound:'
+    print p[lower_bound]
+
+  length_table = numpy.zeros(shape=(m+1,n+1))
+  direction_table = numpy.chararray((m+1, n+1))
   direction_table[:] = '0'
 
   for (x,y), value in numpy.ndenumerate(direction_table):
     if x >= 1 and y >= 1:
-      if A[x-1] == B[y-1]:
+      if A[x-1] == original_B[y-1]:
         length_table[x,y] = length_table[x-1,y-1] + 1
         direction_table[x,y] = '\\'
       elif length_table[x-1,y] >= length_table[x,y-1]:
@@ -71,51 +87,42 @@ def single_shortest_path(p,mid,double_A,B,original_height,upper_bound,lower_boun
         length_table[x,y] = length_table[x,y-1]
         direction_table[x,y] = '<'
 
-  p_mid = print_LCS_nonrecursively(direction_table,A,m-1,n-1,mid)
+  p_mid = print_LCS_nonrecursively(direction_table,A,m,n,mid)
   for value in p_mid:
     preallocated_table[value[0],value[1]] = str(mid+1)
-  for x in range(n-1):
-    direction_table[0,x+1] = B[x]
-  for y in range(m-1):
+  for x in range(n):
+    direction_table[0,x+1] = original_B[x]
+  for y in range(m):
     direction_table[y+1,0] = A[y%m]
+  print '\n\np_mid[' + str(mid) + ']:'
   print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print 'p_mid:'
   print p_mid
-  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print 'direction_table:'
+  print '\ndirection_table for ' + A + ' x ' + original_B + ':'
+  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
   print direction_table
-  print '\n~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print 'preallocated_table:'
+  print '\npreallocated_table:'
+  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
   print preallocated_table
   return p_mid
 
 
 def print_LCS_nonrecursively(direction_table,A,x,y,mid):
   path_i = []
-  subsequence = ''
   while x != 0 and y != 0:
     path_i.append((x+mid,y))
+    if x == 1:
+      break
     if direction_table[x,y] == '\\':
-      subsequence += A[x-1]
       x = x - 1
       y = y - 1
     elif direction_table[x,y] == '^':
       x = x - 1
     else:
       y = y - 1
+  while x == 1 and y > 1:
+    y = y - 1
+    path_i.append((x+mid,y))
   return path_i
-
-
-def print_LCS(direction_table,A,x,y,LCS):
-  if x == 0 or y == 0:
-    return
-  if direction_table[x,y] == '\\':
-    print_LCS(direction_table,A,x-1,y-1,LCS)
-    LCS.append(A[x-1])
-  elif direction_table[x,y] == '^':
-    print_LCS(direction_table,A,x-1,y,LCS)
-  else:
-    print_LCS(direction_table,A,x,y-1,LCS)
 
 
 if __name__ == '__main__':
