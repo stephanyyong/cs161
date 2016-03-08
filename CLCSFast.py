6bf9d2aa89f628aa11e2641155604312f9e8ef9d
Vicki Lau <vickilau@corn39.stanford.edu>
2016-03-07 23:44:31 -0800

    DONEEEEEE

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,25 +1,25 @@
-import LCS
+#Lea Coligado, 05785578, coligado
+#Vicki Lau, 05789700, vickilau
+#Stephany Yong, 05786562, syong
+
 import sys
 import numpy
 
 def main (): 
-  if len(sys.argv) != 2:
-    sys.exit('Usage: `python LCS.py < input`')
+  if len(sys.argv) != 1:
+    sys.exit('Usage: `python CLCSFast.py < input`')
+  for l in sys.stdin:
+     A,B = l.split()
+     print CLCS(A,B)
+  return
 
-  with open(sys.argv[1]) as f: 
-    for line in f: 
-      A,B = line.split()
-      preallocate_table(A,B)
-      return
-
-
-def preallocate_table(A,B):
+def CLCS(A,B):
   global preallocated_table
+  global subsequence_lengths
   global double_A
   global original_B
   global m
   global n
-  global subsequence_lengths
   m = len(A)
   n = len(B)
   double_A = A + A
@@ -34,88 +34,83 @@ def preallocate_table(A,B):
   for y in range(double_m):
     preallocated_table[y+1,0] = A[y%m]
 
-  # initialzie the list of paths p_i
+  # initialize the list of paths p_i
   p = [[] for i in range(m)]
   subsequence_lengths = [0 for i in range(m)]
-  p[0] = single_shortest_path(p,0,-1,-1)
-  p[m-1] = single_shortest_path(p,m-1,-1,-1)
-  find_shortest_paths(p,0,m-1)
-  # print '\nHere are all paths:'
-  # print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  # for i in p:
-  #   print i
-  # print '\nHere are all subsequence_lengths:'
-  # print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  # for i in subsequence_lengths:
-  #   print i
-  # print '\nHere is the max subsequence_length for ' + A + ' x ' + B
-  # print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print max(subsequence_lengths)
+  p[0] = single_shortest_path(p, 0, -1, -1)
+  p[m-1] = single_shortest_path(p, m-1, -1, -1)
+  find_shortest_paths(p, 0, m-1)
+  return max(subsequence_lengths)
 
-
-def find_shortest_paths(p,upper_bound,lower_bound):
-  # print 'find_shortest_paths with upper_bound: ' + str(upper_bound) + ' and l: ' + str(lower_bound)
+def find_shortest_paths(p, upper_bound, lower_bound):
   if lower_bound - upper_bound <= 1:
     return
   mid = (upper_bound+lower_bound)/2
-  p[mid] = single_shortest_path(p,mid,upper_bound,lower_bound)
-  #return
-  find_shortest_paths(p,mid,lower_bound)
-  find_shortest_paths(p,upper_bound,mid)
+  p[mid] = single_shortest_path(p, mid, upper_bound, lower_bound)
+  find_shortest_paths(p, mid, lower_bound)
+  find_shortest_paths(p, upper_bound,mid)
 
-
-def single_shortest_path(p,mid,upper_bound,lower_bound):
+def single_shortest_path(p, mid, upper_bound, lower_bound):
   global preallocated_table
   global double_A
   global original_B
   global m
   global n
   A = double_A[mid:mid+m]
-
   length_table = numpy.zeros(shape=(m+1,n+1))
   direction_table = numpy.chararray((m+1, n+1))
   direction_table[:] = '0'
 
   x = 0
   y = 0
+  skipY = float('-inf')
   while x < m+1:
     while y < n+1:
       if x >= 1 and y >= 1:
-        if A[x-1] == original_B[y-1]:
-          length_table[x,y] = length_table[x-1,y-1] + 1
-          direction_table[x,y] = '\\'
-        elif length_table[x-1,y] >= length_table[x,y-1]:
-          length_table[x,y] = length_table[x-1,y]
-          direction_table[x,y] = '^'
-        else:
-          length_table[x,y] = length_table[x,y-1]
-          direction_table[x,y] = '<'
+        if y > skipY:
+            if passed_upper_bound((x+mid, y), p[upper_bound]):
+              length_table[x,y] = float('-inf')
+              break
+            elif passed_lower_bound((x+mid, y), p[lower_bound]):
+              length_table[x,y] = float('-inf')
+              skipY = y
+            else:
+              if A[x-1] == original_B[y-1]:
+                length_table[x,y] = length_table[x-1,y-1] + 1
+                direction_table[x,y] = '\\'
+              elif length_table[x-1,y] >= length_table[x,y-1]:
+                length_table[x,y] = length_table[x-1,y]
+                direction_table[x,y] = '^'
+              else:
+                length_table[x,y] = length_table[x,y-1]
+                direction_table[x,y] = '<'
       y = y + 1
     y = 0
     x = x + 1
+  return print_LCS(direction_table, A, m, n, mid)
 
-  p_mid = list(reversed(print_LCS_nonrecursively(direction_table,A,m,n,mid)))
-  for value in p_mid:
-    preallocated_table[value[0],value[1]] = str(mid+1)
-  for x in range(n):
-    direction_table[0,x+1] = original_B[x]
-  for y in range(m):
-    direction_table[y+1,0] = A[y%m]
-  print '\ndirection_table for ' + A + ' x ' + original_B + ':'
-  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print direction_table
-  print '\npreallocated_table:'
-  print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-  print preallocated_table
-  return p_mid
+def passed_upper_bound(point, upper_bound):
+  if not upper_bound:
+    return False
+  if point[0] < upper_bound[point[1]][-1]: 
+    return True
+  else:
+    return False
 
+def passed_lower_bound(point, lower_bound):
+  if not lower_bound:
+    return False;
+  if point[0] > lower_bound[point[1]][0]:  
+    return True 
+  else:
+    return False
 
-def print_LCS_nonrecursively(direction_table,A,x,y,mid):
+def print_LCS(direction_table, A, x, y, mid):
   global subsequence_lengths
-  path_i = []
+  path_i = [[] for i in range(x+y)]
   count = 0
   while x != 0 and y != 0:
-    path_i.append((x+mid,y))
+    path_i[y].append(x+mid)
     if x == 1:
         break
     if direction_table[x,y] == '\\':
@@ -131,11 +126,10 @@ def print_LCS_nonrecursively(direction_table,A,x,y,mid):
     if direction_table[x,y] == '\\' and intersection_found == False:
       count = count + 1
       intersection_found = True
-    path_i.append((x+mid,y))
+    path_i[y].append(x+mid)
     y = y - 1
   subsequence_lengths[mid] = count
   return path_i
-
 
 if __name__ == '__main__':
   main()
--- CLCSSlow.py
+++ CLCSSlow.py
@@ -1,42 +1,56 @@
-import LCS
+#Lea Coligado, 05785578, coligado
+#Vicki Lau, 05789700, vickilau
+#Stephany Yong, 05786562, syong
+
 import sys
+import numpy as np
 
-def main (): 
-	if len(sys.argv) != 2:
-		sys.exit('Usage: `python LCS.py < input`')
+arr = np.zeros((2048, 2048), dtype=int)
 
-	with open(sys.argv[1]) as f: 
-		for line in f: 
-			print line
-			A,B = line.split()
+def main():
+  if len(sys.argv) != 1:
+    sys.exit('Usage: `python CLCSSlow.py < input`')
 
-			CLCS_slow (A, B)
+  for l in sys.stdin:
+    A,B = l.split()
+    print CLCS(A,B)
+  return
 
+def LCS(A,B):
+  m = len(A)
+  n = len(B)
 
-def CLCS_slow (A,B):
-	len_A = len(A)
-	len_B = len(B)
-	if len_A > len_B:
-		shorter = B
-		longer = A
-	else:
+  for i in range(1,m+1):
+    for j in range(1,n+1):
+      if A[i-1] == B[j-1]:
+        arr[i][j] = arr[i-1][j-1]+1
+      else:
+        arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+  return arr[m][n]
+
+def CLCS(A,B):
+  len_A = len(A)
+  len_B = len(B)
+  if len_A > len_B:
+	  shorter = B
+	  longer = A
+  else:
 		shorter = A
 		longer = B
 
-	max_seq_len = 0 
+  max_seq_len = 0 
 
-	for i in range (0, len(shorter)):
-		curr_lcs_len = LCS.LCS(cut(shorter, i), longer)
-		if curr_lcs_len > max_seq_len:
-			max_seq_len = curr_lcs_len
+  for i in range (0, len(shorter)):
+	  curr_lcs_len = LCS(cut(shorter, i), longer)
+	  if curr_lcs_len > max_seq_len:
+		  max_seq_len = curr_lcs_len
 
-	print max_seq_len
-
+  return max_seq_len
 
 def cut (shorter, i):
 	new_str = shorter[i:] + shorter[:i]
 	return new_str
-
 
 if __name__ == '__main__':
 	main()
